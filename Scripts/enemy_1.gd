ebc81579f55d48f6f18bff953ac3f72f70a6f420
GoncasCC <goncalo.cor@gmail.com>
2025-08-23 00:21:36 +0100

    Finisher attack + dash

--- Scripts/enemy_1.gd
+++ Scripts/enemy_1.gd
@@ -1,22 +1,35 @@
 extends CharacterBody2D
 
 
-@export var health: int = 40;
-@export var max_health: int = 40;
+@export var health: int = 60;
+@export var max_health: int = 60;
 
 @onready var health_bar: ProgressBar = $Health_bar;
+@onready var label_finisher: Label = $Label_finisher;
+
+var can_be_executed : bool = false;
 
 func _ready():
 	health_bar.max_value = max_health;
 	health_bar.value = health;
+	label_finisher.visible = false;
 
 func take_damage(damage: int) -> void:
 	health -= damage
 	print("Enemy has taken ", damage, " damage. Remaining health: ", health)
 	health_bar.value = health;
+	
+	if health <= 10 and health > 0:
+		can_be_executed = true
+		label_finisher.visible = true
 	if health <= 0:
 		die()
 
+func execute():
+	print("Enemy executed")
+	die()
+	
+	
 func die() -> void:
 	queue_free() # removes enemy from scene
 
--- Scripts/pope.gd
+++ Scripts/pope.gd
@@ -10,47 +10,107 @@ const JUMP_VELOCITY = -400.0
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 
-@export var damage_melee : int = 20;
+#Melee atack
+@export var damage_melee1 : int = 15;
+@export var damage_melee2 : int = 15;
+@export var damage_melee3 : int = 20;
+var combo_step: int = 0
+var combo_timer: float = 0.0
+const COMBO_MAX_TIME = 0.5 # max time between combo attacks
 @onready var hitbox_melee : Area2D = $HitboxMelee;
 
+
+#Ranged attack
 @export var projetile_scene : PackedScene;
 @export var damage_ranged : int = 10;
+
+
+#Dash
+@export var dash_speed: float = 600.0
+@export var dash_duration: float = 0.2
+@export var dash_cooldown: float = 0.5
+var dash_direction: int = 1
+var is_dashing: bool = false
+var dash_time: float = 0.0
+var dash_cd_time: float = 0.0
+
 
 
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_up") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
+		
+	if is_dashing:
+		dash_time -= delta
+		if dash_time > 0:
+			velocity.x = dash_direction * dash_speed
+		else:
+			end_dash()
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		#Handle normal walk
+		var direction = Input.get_axis("ui_left", "ui_right")
+		if direction:
+			velocity.x = direction * SPEED
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
+		# Handle jump.
+		if Input.is_action_just_pressed("ui_up") and is_on_floor():
+			velocity.y = JUMP_VELOCITY
+		#Handle Dash
+		if dash_cd_time > 0:
+			dash_cd_time -= delta
+		if Input.is_action_just_pressed("ui_dash") and dash_cd_time <= 0:
+			start_dash()
 
 	move_and_slide()
 	
 	# Melee attack
 	if Input.is_action_just_pressed("ui_melee"):
 		attack_melee();
+	
 		
 	# Ranged attack
 	if Input.is_action_just_pressed("ui_ranged"):
 		attack_ranged();
-		
-		
-		
+	
+	# reset combo if too much time has passed
+	if combo_timer > 0:
+		combo_timer -= delta
+		if combo_timer <= 0:
+			combo_step = 0
+			
+	if Input.is_action_just_pressed("ui_execute"):
+		for body in hitbox_melee.get_overlapping_bodies():
+			if body.has_method("execute") and body.can_be_executed:
+				body.execute()
+	
+	
+#Melee method
 func attack_melee() -> void:
+	var damage = 0;
+	
+	if combo_step == 0:
+		damage = damage_melee1
+	elif combo_step == 1:
+		damage = damage_melee2
+	elif combo_step == 2:
+		damage = damage_melee3
+	
 	for body in hitbox_melee.get_overlapping_bodies():
 		if body.has_method("take_damage"):
-			body.take_damage(damage_melee);
+			body.take_damage(damage);
+	
+	# Advances combo
+	combo_step += 1
+	if combo_step > 2:
+		combo_step = 0 # after third hit goes back to beggining
+	
+	# Combo timer
+	combo_timer = COMBO_MAX_TIME
+
 			
+#Ranged method
 func attack_ranged():
 	var proj = projetile_scene.instantiate();
 	get_parent().add_child(proj);
@@ -65,3 +125,21 @@ func attack_ranged():
 	
 		
 	proj.damage = damage_ranged;
+	
+#Dash methods
+func start_dash() -> void:
+	print("Dash started");
+	is_dashing = true
+	dash_time = dash_duration
+	dash_cd_time = dash_cooldown
+
+	# Decides direction of the dash
+	dash_direction = sign(velocity.x)
+	if dash_direction == 0:
+		dash_direction = 1
+
+	# Optional: imune during dash?
+
+func end_dash() -> void:
+	print("Dash ended")
+	is_dashing = false
